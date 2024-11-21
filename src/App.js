ac13872c38e67f188c15704737aa624eb550b176
Meng Teng <teng.me@northeastern.edu>
2024-11-20 23:02:20 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationComponent from './NavigationComponent';
+import Home from './Home';
+import Profile from './Profile';
+import Tasks from './Tasks';
+import ResponsiveForm from './ResponsiveForm';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const handleLogin = () => setIsAuthenticated(true);
+    const handleLogout = () => setIsAuthenticated(false);
+
+    return (
+        <Router>
+            {/* Navigation Bar */}
+            <NavigationComponent
+                isAuthenticated={isAuthenticated}
+                handleLogin={handleLogin}
+                handleLogout={handleLogout}
+            />
+            <div className="container mt-4">
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/tasks" element={<Tasks />} />
+                    <Route path="/responsiveForm" element={<ResponsiveForm />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
